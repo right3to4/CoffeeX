ad630d608235a795be9e541d280fcdff64be5ecc
jinhyeong <wlsgud0452@naver.com>
2022-10-09 17:14:54 +0900

    매니저 홈화면 추가

--- CoffeeX/src/com/coffeex/staff/MavagerViewOrders.java
+++ CoffeeX/src/com/coffeex/staff/MavagerViewOrders.java
@@ -8,6 +8,9 @@ import java.awt.Color;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JMenuBar;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 
 public class MavagerViewOrders {
 
@@ -41,16 +44,33 @@ public class MavagerViewOrders {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setBounds(100, 100, 900, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBackground(new Color(147, 128, 96));
-		menuBar.setOpaque(true);
-		//menuBar.setBackground(Color.WHITE);
-		menuBar.setBounds(0, 0, 900, 50);
-		frame.getContentPane().add(menuBar);
+		JLabel lbStaffPayManege = new JLabel("사원급여관리");
+		lbStaffPayManege.setVisible(false);
+		JLabel lbStaffInsertDel = new JLabel("사원등록/삭제");
+		lbStaffInsertDel.setVisible(false);
+		
+		JLabel lbStaffManage = new JLabel("사원관리");
+		lbStaffManage.addMouseListener(new MouseAdapter() {
+			int i = 0;
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(i%2 == 1) {
+					lbStaffManage.setForeground(Color.black);
+					lbStaffPayManege.setVisible(true);
+					lbStaffInsertDel.setVisible(true);	
+				}else {
+					lbStaffManage.setForeground(Color.white);
+					lbStaffPayManege.setVisible(false);
+					lbStaffInsertDel.setVisible(false);
+				}
+				i++;
+			}
+		});
 		
 		JLabel lblNewLabel_1 = new JLabel("마이페이지");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
@@ -73,6 +93,12 @@ public class MavagerViewOrders {
 		lblNewLabel_1_1_1.setBounds(456, 0, 140, 50);
 		frame.getContentPane().add(lblNewLabel_1_1_1);
 		
+		lbStaffManage.setHorizontalAlignment(SwingConstants.CENTER);
+		lbStaffManage.setForeground(Color.WHITE);
+		lbStaffManage.setFont(new Font("Lucida Grande", Font.PLAIN, 23));
+		lbStaffManage.setBounds(304, 0, 140, 50);
+		frame.getContentPane().add(lbStaffManage);
+		
 		JLabel lblNewLabel = new JLabel("  CoffeeX");
 		lblNewLabel.setOpaque(true);
 		lblNewLabel.setForeground(Color.WHITE);
@@ -80,5 +106,32 @@ public class MavagerViewOrders {
 		lblNewLabel.setBackground(new Color(148, 128, 96));
 		lblNewLabel.setBounds(0, 0, 900, 50);
 		frame.getContentPane().add(lblNewLabel);
+		
+//		JLabel lbStaffPayManege = new JLabel("사원급여관리");
+		lbStaffPayManege.setHorizontalAlignment(SwingConstants.CENTER);
+		lbStaffPayManege.setForeground(Color.BLACK);
+		lbStaffPayManege.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		lbStaffPayManege.setBounds(304, 50, 140, 28);
+		frame.getContentPane().add(lbStaffPayManege);
+		
+		
+		lbStaffInsertDel.setHorizontalAlignment(SwingConstants.CENTER);
+		lbStaffInsertDel.setForeground(Color.BLACK);
+		lbStaffInsertDel.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		lbStaffInsertDel.setBounds(304, 79, 140, 28);
+		frame.getContentPane().add(lbStaffInsertDel);
+		
+		JLabel lblNewLabel_2 = new JLabel("직급");
+		lblNewLabel_2.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblNewLabel_2.setBounds(10, 62, 97, 30);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("이름");
+		lblNewLabel_2_1.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblNewLabel_2_1.setBounds(10, 100, 125, 28);
+		frame.getContentPane().add(lblNewLabel_2_1);
+		
+		
+		
 	}
 }
