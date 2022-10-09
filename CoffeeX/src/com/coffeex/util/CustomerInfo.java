45a78c00c1b407bbfeef69dae90c23bf1dc990eb
jinhyeong <wlsgud0452@naver.com>
2022-10-09 12:03:20 +0900

    util추가

---
+++ CoffeeX/src/com/coffeex/util/CustomerInfo.java
@@ -0,0 +1,12 @@
+package com.coffeex.util;
+
+public class CustomerInfo {
+	
+	//로그인 사용자 ID저
+	public static String userid="mirrorspring";
+	
+	public CustomerInfo() {
+		
+	}
+
+}
---
+++ CoffeeX/src/com/coffeex/util/DBConnect.java
@@ -0,0 +1,12 @@
+package com.coffeex.util;
+
+public class DBConnect {
+
+	public DBConnect() {
+
+	}
+
+	public static final String url_mysql = "jdbc:mysql://127.0.0.1:3306/coffeex?serverTimezone=UTC&characterEncoding=utf8&useSSL=False";
+	public static final String id_mysql = "root";
+	public static final String pw = "qwer1234";
+}
